f70393680f5fc3601b1bdeee8124f997142b027b
Your Name <you@example.com>
2022-06-28 17:51:41 +0300

    Added logs fetch v1

--- src/Pages/Logs.jsx
+++ src/Pages/Logs.jsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import LogsList from "../components/LogsList/LogsList";
 import Hero from "../components/Hero/Hero";
-// import Logo from "../Assets/typedu.png";
-import "./Home.css";
 
-const Home = () => {
-  return (
-    <>
-      <Hero
-        title="Your Logs"
-        subtitle="“Take care of all your memories. For you cannot relive them” – Bob Dylan"
-      />
-    </>
-  );
+const Logs = () => {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/v1/logs`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const resData = await res.json();
+
+    setData(resData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (!data) {
+    return (
+      <>
+        {" "}
+        <Hero
+          title="Your Logs"
+          subtitle="“Take care of all your memories. For you cannot relive them” – Bob Dylan"
+        />
+        <p>Loading...</p>
+      </>
+    );
+  }
+
+  if (data) {
+    return (
+      <>
+        <Hero
+          title="Your Logs"
+          subtitle="“Take care of all your memories. For you cannot relive them” – Bob Dylan"
+        />
+        <LogsList allPosts={data} />;
+      </>
+    );
+  }
 };
 
-export default Home;
+export default Logs;
